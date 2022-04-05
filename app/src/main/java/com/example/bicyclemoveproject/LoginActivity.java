3dd30371843ae02f4b9d0cd8766f123bacf16aa9
Seunghun <qotmdgns123@naver.com>
2022-04-05 15:15:44 +0900

    second commit

--- app/src/main/java/com/example/bicyclemoveproject/LoginActivity.java
+++ app/src/main/java/com/example/bicyclemoveproject/LoginActivity.java
@@ -6,14 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     TextView sign;
 
     @Override
@@ -26,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         //회원가입 버튼 클릭시, 회원가입 페이지로 이동
         sign.setOnClickListener(v -> {
-            Intent intent = new Intent(this, SignupActivity.class);
-            startActivity(intent);
+            //Intent intent = new Intent(this, Signup.class);
+            //startActivity(intent);
         });
     }
 }
